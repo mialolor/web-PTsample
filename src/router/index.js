9e2f4377e15ac0225e8f323c5051e6dfc2bd02d5
LolorJane <maryjanelolor@gmail.com>
2024-10-18 09:38:42 +0800

    feat: added proper folder structure

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import LoginView from '@/views/LoginView.vue'
+import LoginView from '@/views/auth/LoginView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,10 +7,9 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: LoginView
+      component: LoginView,
     },
-  
-  ]
+  ],
 })
 
 export default router
