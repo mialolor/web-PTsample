f46e6a91121c870dbb7a7a3392f0e90908724c0c
mialolor <mialolor4@gmail.com>
2024-10-20 23:46:50 +0800

    feat: added password view func

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -9,37 +9,42 @@ const { mobile } = useDisplay()
 <template>
   <AppLayout>
     <template #content>
-      <v-row>
-        <v-col cols="12" md="6" class="mx-auto pt-16">
-          <v-card class="mx-auto" elevation="24">
-            <v-card-title class="text-center">
-              <v-img
-                class="mx-auto"
-                src="/images/logo-favicon.png"
-                :width="mobile ? '75%' : '50%'"
-              ></v-img>
+      <v-container fluid>
+        <v-row>
+          <v-col cols="12" md="6" class="mx-auto pt-16">
+            <v-card class="mx-auto" elevation="24">
+              <v-card-title class="text-center">
+                <v-img
+                  class="mx-auto"
+                  src="/images/logo-favicon.png"
+                  :width="mobile ? '50%' : '30%'"
+                  style="border-radius: 50%; overflow: hidden"
+                ></v-img>
 
-              <h3 class="font-weight-black text-center">PawTrack</h3>
-              <p class="font-weight-bold">Login Form</p>
-            </v-card-title>
+                <h3 class="font-weight-black text-center">PawTrack</h3>
+                <p class="font-weight-bold">Login Form</p>
+              </v-card-title>
 
-            <v-card-text class="bg-surface-light pt-4">
-              <v-divider class="my-5"></v-divider>
+              <v-card-text class="bg-surface-light pt-4">
+                <v-divider class="my-5"></v-divider>
 
-              <LoginForm></LoginForm>
+                <LoginForm></LoginForm>
 
-              <v-divider class="my-5"></v-divider>
+                <v-divider class="my-5"></v-divider>
 
-              <h5 class="text-center">
-                Don't have account?
-                <RouterLink class="text-yellow-darken-4 font-weight-black" to="/register"
-                  >Click here to Register</RouterLink
-                >
-              </h5>
-            </v-card-text>
-          </v-card>
-        </v-col>
-      </v-row>
+                <h5 class="text-center">
+                  Don't have account?
+                  <RouterLink
+                    class="text-yellow-darken-4 font-weight-black"
+                    to="/register"
+                    >Click here to Register</RouterLink
+                  >
+                </h5>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
     </template>
   </AppLayout>
 </template>
--- src/views/auth/RegisterView.vue
+++ src/views/auth/RegisterView.vue
@@ -4,41 +4,47 @@ import RegisterForm from '@/components/auth/RegisterForm.vue'
 import { useDisplay } from 'vuetify'
 
 const { mobile } = useDisplay()
+
 </script>
 
 <template>
   <AppLayout>
     <template #content>
-      <v-row>
-        <v-col cols="12" md="6" class="mx-auto pt-16">
-          <v-card class="mx-auto" elevation="24"
-            ><v-card-title class="text-center">
-              <v-img
-                class="mx-auto"
-                src="/images/logo-favicon.png"
-                :width="mobile ? '75%' : '50%'"
-              ></v-img>
-              <h3 class="font-weight-black text-center">PawTrack</h3>
-              <p>RegistrationForm</p>
-            </v-card-title>
+      <v-container fluid>
+        <v-row>
+          <v-col cols="12" md="6" class="mx-auto pt-16">
+            <v-card class="mx-auto" elevation="24"
+              ><v-card-title class="text-center">
+                <v-img
+                  class="mx-auto"
+                  src="/images/logo-favicon.png"
+                  :width="mobile ? '50%' : '30%'"
+                  style="border-radius: 50%; overflow: hidden;"
+                ></v-img>
+                <h3 class="font-weight-black text-center">PawTrack</h3>
+                <p><b>Registration Form</b></p>
+              </v-card-title>
 
-            <v-card-text class="bg-surface-light pt-4">
-              <v-divider class="my-5"></v-divider>
+              <v-card-text class="bg-surface-light pt-4">
+                <v-divider class="my-5"></v-divider>
 
-              <RegisterForm></RegisterForm>
+                <RegisterForm></RegisterForm>
 
-              <v-divider class="my-5"></v-divider>
+                <v-divider class="my-5"></v-divider>
 
-              <h5 class="text-center ">
-                Already have account?
-                <RouterLink class="text-yellow-darken-4 font-weight-black" to="/"
-                  >Click here to Login</RouterLink
-                >
-              </h5>
-            </v-card-text>
-          </v-card>
-        </v-col>
-      </v-row>
+                <h5 class="text-center">
+                  Already have account?
+                  <RouterLink
+                    class="text-yellow-darken-4 font-weight-black"
+                    to="/"
+                    >Click here to Login</RouterLink
+                  >
+                </h5>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
     </template>
   </AppLayout>
 </template>
